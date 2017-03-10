bf0fdd597bc4f679562efaa1b3502c7b816b7fca
tlabna <tlabna@users.noreply.github.com>
2017-03-10 15:17:36 -0500

    Add .gitignore (untrack node_modules) and Gulpfile.js. Modify index.html by optimizing css and js statements in head element. (inline css, async javascript)

---
+++ Gulpfile.js
@@ -0,0 +1,5 @@
+var gulp = require('gulp');
+var resizeImg = require('gulp-image-resize');
+var minifyCss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var minifyHTML = require('gulp-minify-html');
